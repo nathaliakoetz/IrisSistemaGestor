13d9bb1d30a4ba3cc37bf80558e0f883a4b7eb98
Guilherme Acosta <gw.acosta1991@gmail.com>
2024-10-10 03:28:44 -0300

    Ajustes na tela de login

--- front/src/app/signin/page.tsx
+++ front/src/app/signin/page.tsx
@@ -6,11 +6,13 @@ export default function SignIn() {
         <section className="bg-[url('/bg_login.jpeg')] bg-cover bg-no-repeat h-full flex justify-center items-center">
             <div className="flex justify-center items-center">
                 <div className="max-w-lg border border-gray-400 bg-white rounded-3xl shadow flex flex-col items-center h-auto overflow-hidden">
-                    <div className="flex flex-row w-full items-center justify-center mt-14">
-                        <img src="./logo.png" alt="Icone do Sistema Íris" className="w-1/4" />
-                        <h1 className={`text-7xl ms-1 text-color-logo ${cairo.className}`}>
-                            ÍRIS
-                        </h1>
+                    <div className="flex flex-row w-card-login items-center justify-center mt-14">
+                        <Link href="/" className="flex">
+                            <img src="./logo.png" alt="Icone do Sistema Íris" className="w-32" />
+                            <h1 className={`text-7xl ms-1 text-color-logo ${cairo.className}`}>
+                                ÍRIS
+                            </h1>
+                        </Link>
                     </div>
                     <form className="max-w-lg mx-auto mt-10">
                         <div className="mb-5">
@@ -18,7 +20,7 @@ export default function SignIn() {
                                 type="email"
                                 id="email"
                                 className={`bg-form border-user text-gray-900 placeholder:text-header-selected text-sm rounded-2xl block w-80 p-2.5 ${inter.className}`}
-                                placeholder="Nome de Usuário ou e-mail"
+                                placeholder="E-mail"
                                 required
                             />
                         </div>
